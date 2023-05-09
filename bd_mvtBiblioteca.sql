1afe076ca647e45575b860f1bf5ad01a0ce4feae
Gustavo Kauan <gustavokauan2002@hotmail.com>
2023-05-09 10:20:51 -0300

    no message

--- bd_mvtBiblioteca.sql
+++ bd_mvtBiblioteca.sql
@@ -23,3 +23,47 @@ create table mvtBibSecao(
 	codSecao int identity primary key,
 	descricaoSecao varchar(50) not null
 );
+
+/*create table mvtBibItemAcervo(
+	codItem int identity primary key,
+	codLocal int not null,
+	numExemplar int not null,
+	nome varchar(200) not null,
+	codAutor int not null,
+	nomeAutor varchar(50) not null,
+	codEditora int not null,
+	nomeEditora varchar(50) not null,
+	tipoItem varchar(50) not null,
+	nomeLocal varchar(50) not null,
+	volume int,
+	anoEdicao int,
+	localização varchar(50) not null,
+	secao varchar(50),
+	idioma varchar(50),
+	foreign key(codLocal) references mvtBibLocal(codLocal),
+	foreign key(codAutor) references mvtBibAutor(codAutor),
+	foreign key(codEditora) references mvtBibEditora(codEditora)
+);*/
+
+create table mvtBibItemAcervo(
+	codItem int identity primary key,
+	codLocal int not null,
+	numExemplar int not null,
+	nome varchar(200) not null,
+	codAutor int not null,
+	nomeAutor varchar(50) not null,
+	codEditora int not null,
+	nomeEditora varchar(50) not null,
+	tipoItem varchar(50) not null,
+	nomeLocal varchar(50) not null,
+	volume int,
+	anoEdicao int,
+	localização varchar(50) not null,
+	codSecao int not null,
+	secao varchar(50),
+	idioma varchar(50),
+	foreign key(codLocal) references mvtBibLocal(codLocal),
+	foreign key(codAutor) references mvtBibAutor(codAutor),
+	foreign key(codEditora) references mvtBibEditora(codEditora),
+	foreign key(codSecao) references mvtBibSecao(codSecao)
+);
